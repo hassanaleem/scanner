a983c0c323b527fb1a2cebdc331129a9fa86f770
unknown <hassanaleem9@gmail.com>
2022-01-01 18:56:11 +0400

    Added Multiple Image Support

--- scanner.py
+++ scanner.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import glob 
 
 size = (3508, 2480) # A4 size
-image = cv2.imread("image.jpg")
+dst = np.array([[0,0],[size[1],0], [0, size[0]], [size[1], size[0]]], dtype=float)
 
 def click_event(event, x, y, flags, params):
 
@@ -14,16 +15,21 @@ def click_event(event, x, y, flags, params):
             pointSrc.append([x, y])
 
 
-pointSrc = list()
+def main_func(image, file):
+    pointSrc = list()
 
-cv2.imshow("image", image)
-cv2.setMouseCallback('image', click_event, [image, pointSrc])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("image", image)
+    cv2.setMouseCallback('image', click_event, [image, pointSrc])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-dst = np.array([[0,0],[size[1],0], [0, size[0]], [size[1], size[0]]], dtype=float)
-pointSrc = np.array(pointSrc)
-M, mask = cv2.findHomography(pointSrc, dst)
-result_img = cv2.warpPerspective(image, M, (size[1], size[0]))
-cv2.imwrite("result.jpg", result_img)
+    pointSrc = np.array(pointSrc)
+    M, mask = cv2.findHomography(pointSrc, dst)
+    result_img = cv2.warpPerspective(image, M, (size[1], size[0]))
+    fname = "result_" + file[:-4] + ".jpg"
+    cv2.imwrite(fname, result_img)
+
+for file in glob.glob("*.jpg"):
+    image = cv2.imread(file)
+    main_func(image, file)
 
