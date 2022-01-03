3118ce4e4785a9b18c82f8cabf31a10fcac825c0
unknown <hassanaleem9@gmail.com>
2022-01-03 15:07:19 +0400

    Added PDF support

--- scanner.py
+++ scanner.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as np
 import glob 
+import img2pdf
+import os
+import time
 
+instructions = """
+Instructions:
+Select the points in the following order Top left, top right, bottom left, bottom right.
+Once the points are selected press esc key to move to the next image.
+"""
+print(instructions)
+time.sleep(2)
 size = (3508, 2480) # A4 size
 dst = np.array([[0,0],[size[1],0], [0, size[0]], [size[1], size[0]]], dtype=float)
 
@@ -14,10 +24,14 @@ def click_event(event, x, y, flags, params):
             cv2.imshow('image', params[0])
             pointSrc.append([x, y])
 
+        if len(pointSrc) == 4:
+            return
+
 
 def main_func(image, file):
     pointSrc = list()
 
+    i = image.copy()
     cv2.imshow("image", image)
     cv2.setMouseCallback('image', click_event, [image, pointSrc])
     cv2.waitKey(0)
@@ -25,11 +39,17 @@ def main_func(image, file):
 
     pointSrc = np.array(pointSrc)
     M, mask = cv2.findHomography(pointSrc, dst)
-    result_img = cv2.warpPerspective(image, M, (size[1], size[0]))
-    fname = "result_" + file[:-4] + ".jpg"
+    result_img = cv2.warpPerspective(i, M, (size[1], size[0]))
+    fname = "result_" + file[:-4] + ".png"
     cv2.imwrite(fname, result_img)
+
 
 for file in glob.glob("*.jpg"):
     image = cv2.imread(file)
     main_func(image, file)
 
+with open("result.pdf","wb") as f:
+    f.write(img2pdf.convert(glob.glob("*.png")))
+
+for file in glob.glob("*.png"):
+    os.remove(file)
